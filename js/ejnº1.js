aa2db841b4cf9b5b1aa592a2d93bf09196a4bb57
EduRossB <edu_barba@outlook.com>
2022-07-26 20:26:39 -0300

    primer commit

---
+++ js/ejnº1.js
@@ -0,0 +1,13 @@
+let auto = {
+    marca: "Ford",
+    modelo: "Focus",
+    color: ["Negro", "Blanco", "Azul", "Gris"],
+    encender: () => {
+        document.write("*sonido de motor*")
+    },
+    apagar: () =>{
+        document.write("*silencio*")
+    },
+    }
+
+
