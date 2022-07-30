6797a738d21d1b9d39c861e41b22d6d18afd2145
EduRossB <edu_barba@outlook.com>
2022-07-30 15:34:47 -0300

    ejercicio 1

--- js/ejnº1.js
+++ js/ejnº1.js
@@ -1,12 +1,16 @@
+let estado = false
+
 let auto = {
     marca: "Ford",
     modelo: "Focus",
     color: ["Negro", "Blanco", "Azul", "Gris"],
     encender: () => {
         document.write("*sonido de motor*")
+        estado = true
     },
     apagar: () =>{
         document.write("*silencio*")
+        estado = false
     },
     }
 
